9623c97a70d89bb76ed44b98ac2dd7a1408b3a39
Simeon99 <simeonilic@yahoo.com>
2025-02-05 17:10:50 +0100

    Fix Experiance hover.

--- src/components/home/ExperianceCard.tsx
+++ src/components/home/ExperianceCard.tsx
@@ -1,18 +1,21 @@
 'use client'
 
 
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { ResponseDataExperiance } from '@/data/experiences'
 
 const ExperianceCard = ({ item, className }: { item: ResponseDataExperiance, className: string }) => {
 
-
+    const [isHovered, setIsHovered] = useState(false);
     // const handleClick = () => {
 
     // }
     return (
-        <Link href={`/experience/${item.id}`} className={`border-t-[1px] ${className} border-white py-4  text-white hover:bg-green active:bg-green  hover:text-black hover:cursor-pointer`}>
+        <Link href={`/experience/${item.id}`} className={`border-t-[1px] ${className} border-white py-4  text-white  sm:hover:bg-green active:bg-green  sm:hover:text-black  hover:cursor-pointer`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        >
             <div className='grid grid-cols-4 max-[980px]:grid-cols-1 px-8 max-[980px]:px-4 font-berghan  place-content-center justify-start '>
                 <div className='flex flex-col align-middle justify-center max-[980px]:hidden '>
                     <span className='text-[32px] '>
@@ -22,9 +25,9 @@ const ExperianceCard = ({ item, className }: { item: ResponseDataExperiance, cla
                         {item.duration}
                     </span>
                 </div>
-                <div className='text-[32px] flex items-center  max-[980px]:text-green leading-none'>{item.companyName} </div>
+                <div className={`text-[32px] flex items-center  max-[980px]:text-green  ${isHovered ? 'max-[980px]:text-black': ''} leading-none`}>{item.companyName} </div>
                 <div className='flex-row items-center  gap-2 align-middle hidden max-[980px]:flex'>
-                    <div className='h-[70%] w-[2px] bg-white'>
+                    <div className={`h-[70%] w-[2px] bg-white ${isHovered ? 'max-[980px]:bg-black': ''} leading-none`}>
 
                     </div>
                     <div className='flex flex-col'>
