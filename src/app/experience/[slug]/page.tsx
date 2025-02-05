d0ee73e623a04a03b9a41859d27f7e0a15d68323
Simeon99 <simeonilic@yahoo.com>
2025-02-05 16:16:16 +0100

    Add preview image

--- src/app/experience/[slug]/page.tsx
+++ src/app/experience/[slug]/page.tsx
@@ -6,18 +6,6 @@ import PageClient from './PageClient'
 export const metadata = {
   title: "Experience",
   description: "Explore my professional experience and skills.",
-  keywords: "full-stack developer, React developer, Next.js, .NET, Spring Boot, web development, software engineer, portfolio",
-  openGraph: {
-    title: "Simeon Ilić - Full-Stack Developer",
-    description: "I build modern web applications using React, Next.js, and .NET. Check out my portfolio and see my latest projects.",
-    url: "https://www.simeonilic.dev",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Simeon Ilić - Full-Stack Developer",
-    description: "I build modern web applications using React, Next.js, and .NET. Check out my portfolio and see my latest projects.",
-  },
 };
 
 const Page = async  ({
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,26 @@ import Footer from "@/components/footer/Footer";
 export const metadata: Metadata = {
   title: "Simeon Ilić - Full Stack Developer",
   description: "Explore my portfolio and projects in web development.",
+  keywords: "full-stack developer, React developer, Next.js, .NET, Spring Boot, web development, software engineer, portfolio",
+  openGraph: {
+    title: "Simeon Ilić - Full-Stack Developer",
+    description: "I build modern web applications using React, Next.js, and .NET. Check out my portfolio and see my latest projects.",
+    url: "https://www.simeonilic.dev",
+    type: "website",
+    images: [
+      {
+        url: "https://simeonilic.dev/images/thumbnail.png", // ✅ Full URL
+        width: 1200,
+        height: 630,
+        alt: "My Portfolio - Projects by Simeon Ilić",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Simeon Ilić - Full-Stack Developer",
+    description: "I build modern web applications using React, Next.js, and .NET. Check out my portfolio and see my latest projects.",
+  },
 };
 
 export default function RootLayout({
